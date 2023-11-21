5bb58d4c7a17b9da0b386af4d34e7b6756280621
Hicham <hicham@Hichams-MacBook-Pro.local>
2023-11-21 09:42:00 +0100

    first commit

---
+++ src/test/java/com/example/stockifybackend/StockifyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stockifybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockifyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
