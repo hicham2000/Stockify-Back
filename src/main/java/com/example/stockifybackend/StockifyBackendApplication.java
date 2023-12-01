9cc0c2387c702d7e904a0a4f31a3e87719b92e9a
Hicham <hicham@Hichams-MacBook-Pro.local>
2023-12-01 21:28:28 +0100

    remove Application tests

--- src/main/java/com/example/stockifybackend/StockifyBackendApplication.java
+++ src/main/java/com/example/stockifybackend/StockifyBackendApplication.java
@@ -36,44 +36,44 @@ public class StockifyBackendApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        CategorieDeProduits categorieDeProduits = new CategorieDeProduits();
-        categorieDeProduits.setIntitule("hicham");
-
-        ValeurNutritionnel valeur = new ValeurNutritionnel(1L,3,4,6,7,3,4);
-        this.valeurNutritionnelRepository.save(valeur);
-
-        Stock stock = new Stock();
-        this.stockRepository.save(stock);
-
-
-
-
-
-        Recette recette = new Recette();
-        recette.setIntitule("fghj");
-        recette.setStock(stock);
-
-        this.recetteRepository.save(recette);
-
-        Ingredient ingredient = new Ingredient();
-        ingredient.setIntitule("kamoun");
-        ingredient.setRecette(recette);
-
-        ingredientRepository.save(ingredient);
-
-
-
-
-
-
-
-        Produit p = new Produit("testproduct1");
-        p.setIntitule("ghj");
-        p.setValeurNutritionnel(valeur);
-        p.setStock(stock);
-        this.produitRepository.save(p);
-        categorieDeProduits.setProduit(p);
-        this.categorieDeProduitsRepository.save(categorieDeProduits);
+//        CategorieDeProduits categorieDeProduits = new CategorieDeProduits();
+//        categorieDeProduits.setIntitule("hicham");
+//
+//        ValeurNutritionnel valeur = new ValeurNutritionnel(1L,3,4,6,7,3,4);
+//        this.valeurNutritionnelRepository.save(valeur);
+//
+//        Stock stock = new Stock();
+//        this.stockRepository.save(stock);
+//
+//
+//
+//
+//
+//        Recette recette = new Recette();
+//        recette.setIntitule("fghj");
+//        recette.setStock(stock);
+//
+//        this.recetteRepository.save(recette);
+//
+//        Ingredient ingredient = new Ingredient();
+//        ingredient.setIntitule("kamoun");
+//        ingredient.setRecette(recette);
+//
+//        ingredientRepository.save(ingredient);
+//
+//
+//
+//
+//
+//
+//
+//        Produit p = new Produit("testproduct1");
+//        p.setIntitule("ghj");
+//        p.setValeurNutritionnel(valeur);
+//        p.setStock(stock);
+//        this.produitRepository.save(p);
+//        categorieDeProduits.setProduit(p);
+//        this.categorieDeProduitsRepository.save(categorieDeProduits);
 
 
 
