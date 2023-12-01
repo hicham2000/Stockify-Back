9017c2e2debdb58a600fa4b4cb3401f7ca3550e1
Hicham <hicham@Hichams-MacBook-Pro.local>
2023-12-01 21:39:45 +0100

    ProduitAAcheter entity and repository

---
+++ src/main/java/com/example/stockifybackend/Repositories/ProduitAAcheterRepository.java
@@ -0,0 +1,8 @@
+package com.example.stockifybackend.Repositories;
+
+
+import com.example.stockifybackend.Entities.ProduitAAcheter;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProduitAAcheterRepository extends JpaRepository<ProduitAAcheter, Long> {
+}
