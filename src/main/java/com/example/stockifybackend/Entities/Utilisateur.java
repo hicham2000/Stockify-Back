48c056879d31d594c78fb70fe5180c9cc819777d
marouane <marouane.qaffssaoui@uit.ac.ma>
2023-12-02 13:35:27 +0100

    Class Utilisateur

---
+++ src/main/java/com/example/stockifybackend/Entities/Utilisateur.java
@@ -0,0 +1,18 @@
+package com.example.stockifybackend.Entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Utilisateur {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String prénom;
+    private String nom;
+    private String email;
+    private String password;
+    private boolean modeSportif;
+}
