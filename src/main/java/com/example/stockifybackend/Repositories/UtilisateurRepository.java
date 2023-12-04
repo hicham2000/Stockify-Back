36dad9a40d336e86b8c6f9747a1f30f255642c2d
marouane <marouane.qaffssaoui@uit.ac.ma>
2023-12-04 12:53:43 +0100

    je cree utilisateur repository

---
+++ src/main/java/com/example/stockifybackend/Repositories/UtilisateurRepository.java
@@ -0,0 +1,8 @@
+package com.example.stockifybackend.Repositories;
+
+import com.example.stockifybackend.Entities.Utilisateur;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UtilisateurRepository extends JpaRepository<Utilisateur, Long> {
+
+}
