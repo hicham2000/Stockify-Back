330c04759cdb7644cc4758108d3f0d8104e4e466
marouane <marouane.qaffssaoui@uit.ac.ma>
2023-12-04 15:27:54 +0100

    j'ajoute des changements dans la classe Utilisateur service

--- src/main/java/com/example/stockifybackend/services/UtilisateurService.java
+++ src/main/java/com/example/stockifybackend/services/UtilisateurService.java
@@ -1,11 +1,36 @@
 package com.example.stockifybackend.services;
 
+import com.example.stockifybackend.Entities.Utilisateur;
 import com.example.stockifybackend.Repositories.UtilisateurRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UtilisateurService {
     @Autowired
     private UtilisateurRepository utilisateurRepository;
+
+    // add an utilisateur
+    public Utilisateur addUtilisateur(Utilisateur utilisateur) {
+        return utilisateurRepository.save(utilisateur);
+    }
+    // delete an utilisateur
+    public void deleteUtilisateur(Long id) {
+        Optional<Utilisateur> tempUtilisateur = utilisateurRepository.findById(id);
+        if (tempUtilisateur.isEmpty()) {
+            throw new RuntimeException("Utilisateur with id {\"+ id +\"} not found");
+        }
+        utilisateurRepository.delete(tempUtilisateur.get());
+    }
+    // get Utilisateur by id
+    public Utilisateur getUtilisateur(Long id) {
+        Optional<Utilisateur> tempUtilisateur = utilisateurRepository.findById(id);
+        if (tempUtilisateur.isEmpty()) {
+            throw new RuntimeException("Utilisateur with id {\"+ id +\"} not found");
+        }
+        return tempUtilisateur.get();
+    }
+
 }
