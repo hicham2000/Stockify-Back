94b34322317bc0220abd12b45634b389de17e58c
unknown <w2001-RF>
2024-01-05 13:39:05 +0100

    supprimer l'héritage du RecetteRepository depuis  PaginAndSortingRepository pour la pagination

--- src/main/java/com/example/stockifybackend/Repositories/RecetteRepository.java
+++ src/main/java/com/example/stockifybackend/Repositories/RecetteRepository.java
@@ -5,11 +5,10 @@ import com.example.stockifybackend.Entities.Produit;
 import com.example.stockifybackend.Entities.Recette;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
 
-public interface RecetteRepository extends PagingAndSortingRepository<Recette, Long> {
+public interface RecetteRepository extends JpaRepository<Recette, Long>  {
     @Query("SELECT r FROM Recette r WHERE r.stock.id = :stockId")
     List<Recette> findAllByStockIdCustomQuery(Long stockId);
 }
