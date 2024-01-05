0b4c2b2753dc388b7abe21554dd0c440d67c2767
unknown <w2001-RF>
2024-01-06 00:58:29 +0100

    créer la méthode getRecetteByID au conrolleur du recette

--- src/main/java/com/example/stockifybackend/Controllers/RecetteController.java
+++ src/main/java/com/example/stockifybackend/Controllers/RecetteController.java
@@ -12,11 +12,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-
 @RestController
 @RequestMapping("/api")
 public class RecetteController {
