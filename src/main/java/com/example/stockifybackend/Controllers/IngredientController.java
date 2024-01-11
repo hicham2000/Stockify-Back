c9ce6df7ad8a8e1ef05b6d64578d4d35d84f67bd
unknown <w2001-RF>
2024-01-11 16:31:52 +0100

    add script to Add Recipes data script

---
+++ src/main/java/com/example/stockifybackend/Controllers/IngredientController.java
@@ -0,0 +1,40 @@
+package com.example.stockifybackend.Controllers;
+
+import com.example.stockifybackend.Entities.Ingredient;
+import com.example.stockifybackend.Entities.Recette;
+import com.example.stockifybackend.Repositories.IngredientRepository;
+import com.example.stockifybackend.Repositories.RecetteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api")
+public class IngredientController {
+
+    @Autowired
+    private IngredientRepository ingredientRepository;
+
+    @GetMapping("/Ingredient/{ingredientId}")
+    public ResponseEntity<?> getIngredientByID(@PathVariable Long ingredientId) {
+        Map<String, Object> response = new HashMap<>();
+        Optional<Ingredient> OptionalIngredient = ingredientRepository.findById(ingredientId);
+
+        if(OptionalIngredient.isPresent()){
+            Ingredient ingredient = OptionalIngredient.get();
+            response.put("message", "Ingredient récupérer par succès");
+            response.put("ingredient", ingredient);
+        }
+
+        response.put("message", "Aucun Ingredient avec id=" + ingredientId);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+}
